d4a3d5a3d0d4b01905de7944a1f1995c1efebd0c
sathwik-14 <kywagle@gmail.com>
2023-08-08 22:52:50 +0530

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
+import { LandingPageComponent } from './components/landingpage/landing-page.component';
 
 const routes: Routes = [
-  {path: '', component: SigninComponent}
+  {path: '', title: 'Netflix Inida-Watch TV Show', component: LandingPageComponent},
+  {path: 'signin',title: 'Netflix', component: SigninComponent}
 ];
 
 @NgModule({
