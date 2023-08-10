247fe51a9dea66d30b90b3a51ab2c07d4e2e20e5
sathwik-14 <kywagle@gmail.com>
2023-08-10 23:36:30 +0530

    added 4 more section to landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SigninComponent } from './components/signin/signin.component';
-import { LandingPageComponent } from './components/landingpage/landing-page.component';
 
 const routes: Routes = [
-  {path: '', title: 'Netflix Inida-Watch TV Show', component: LandingPageComponent},
-  {path: 'signin',title: 'Netflix', component: SigninComponent}
+  {path: '', title: 'Netflix Inida-Watch TV Show', loadComponent:()=>import('./components/landingpage/landing-page.component').then(e=>e.LandingPageComponent)},
+  {path: 'signin',title: 'Netflix', loadComponent:()=> import('./components/signin/signin.component').then(e=>e.SigninComponent)}
 ];
 
 @NgModule({
