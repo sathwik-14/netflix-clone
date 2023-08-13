0a3e03257578c9122cc1b0a3f5d7d8c358067254
sathwik-14 <kywagle@gmail.com>
2023-08-13 20:27:01 +0530

    changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', title: 'Netflix India-Watch TV Show', loadComponent:()=>import('./components/landingpage/landing-page.component').then(e=>e.LandingPageComponent)},
-  {path: 'signin',title: 'Netflix', loadComponent:()=> import('./components/signin/signin.component').then(e=>e.SigninComponent)}
+  {path: '/signin',title: 'Netflix', loadComponent:()=> import('./components/signin/signin.component').then(e=>e.SigninComponent)}
 ];
 
 @NgModule({
