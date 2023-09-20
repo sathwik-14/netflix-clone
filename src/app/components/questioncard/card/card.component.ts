11ce03d513191d734d70347287c9446e4753f699
sathwik-14 <kywagle@gmail.com>
2023-09-20 20:45:48 +0530

    last commit

--- src/app/components/questioncard/card/card.component.ts
+++ src/app/components/questioncard/card/card.component.ts
@@ -5,7 +5,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-card',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './card.component.html',
+  template: 
+  `
+  <div class="question-card">
+  <div class="d-flex question" (click)="showAnswer = !showAnswer">
+    <div class="h4">{{ question }}</div>
+    <div class="icon h4 ms-auto" [ngClass]="showAnswer ? 'rotate' : ''">
+      <i class="fa fa-xl fa-plus"></i>
+    </div>
+  </div>
+  <div class="h4 answer" [ngClass]="showAnswer ? 'show' : 'hide'">
+    {{ answer }}
+  </div>
+</div>
+
+  `,
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
